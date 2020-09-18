605218eee46c1849a84beede855f8b51c4cb7767
dingshujie <913292259@qq.com>
2020-09-18 21:59:11 +0800

    接口测试

--- user/src/main/java/com/hqc/user/controller/HqcDepartmentUserController.java
+++ user/src/main/java/com/hqc/user/controller/HqcDepartmentUserController.java
@@ -1,9 +1,13 @@
 package com.hqc.user.controller;
 
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * <p>
@@ -16,5 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user/hqc-department-user")
 public class HqcDepartmentUserController {
+
+    @GetMapping("/test")
+    public ModelAndView user(Model model, @PathVariable Long id) {
+
+        ModelAndView mav=new ModelAndView();
+        mav.addObject("message", "SpringBoot 大爷你好！");
+        mav.setViewName("helloWorld");
+        return mav;
+    }
 
 }
