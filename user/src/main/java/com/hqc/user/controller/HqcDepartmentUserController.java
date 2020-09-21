27efe7e6219608416d595490be47b2647bfc8ba0
dingshujie <913292259@qq.com>
2020-09-21 11:48:14 +0800

    引入freemarker及调试

--- user/src/main/java/com/hqc/user/controller/HqcDepartmentUserController.java
+++ user/src/main/java/com/hqc/user/controller/HqcDepartmentUserController.java
@@ -1,13 +1,10 @@
 package com.hqc.user.controller;
 
 
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -18,18 +15,14 @@ import org.springframework.web.servlet.ModelAndView;
  * @author kingmodeyu
  * @since 2020-09-17
  */
-@RestController
+@Controller
 @RequestMapping("/user/hqc-department-user")
 public class HqcDepartmentUserController {
 
-    @GetMapping("/test")
-    public ModelAndView user(Model model, @PathVariable Long id) {
-
-        ModelAndView mav=new ModelAndView();
-        mav.addObject("message", "SpringBoot 大爷你好！");
-        mav.setViewName("helloWorld");
-        mav.setStatus(HttpStatus.ACCEPTED);
-        return mav;
+    @RequestMapping("/test")
+    public String user(Model model) {
+        model.addAttribute("welcome","hello fishpro");
+        return "index";
     }
 
 }
