8459d76297d211b49a1b6b8fdce5273b738c5d88
dingshujie <913292259@qq.com>
2020-09-23 21:52:42 +0800

    user 新增删除功能实现

--- user/src/main/java/com/hqc/user/controller/HqcDepartmentUserController.java
+++ user/src/main/java/com/hqc/user/controller/HqcDepartmentUserController.java
@@ -1,7 +1,12 @@
 package com.hqc.user.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hqc.user.entity.HqcDepartmentUser;
+import com.hqc.user.service.IHqcDepartmentUserService;
+import com.hqc.user.service.impl.HqcDepartmentUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +25,9 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/user/hqc-department-user")
 public class HqcDepartmentUserController {
 
+    @Autowired
+    private IHqcDepartmentUserService hqcDepartmentUserService;
+
     @RequestMapping("/test")
     public String user(Model model) {
         model.addAttribute("welcome","hello fishpro");
@@ -29,7 +37,15 @@ public class HqcDepartmentUserController {
     @RequestMapping("/addUser")
     public String addUser(@RequestBody HqcDepartmentUser user) {
         //model.addAttribute("welcome","hello fishpro");
+        hqcDepartmentUserService.save(user);
+        return "index";
+    }
 
+    @RequestMapping("/delUser")
+    public String delUser(@RequestBody HqcDepartmentUser user) {
+        //model.addAttribute("welcome","hello fishpro");
+        Wrapper<HqcDepartmentUser> queryWrapper = new QueryWrapper<HqcDepartmentUser>(user);
+        hqcDepartmentUserService.remove(queryWrapper);
         return "index";
     }
 
