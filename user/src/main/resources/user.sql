1cae73be94c19b404cb84be6dc6753b8536cf462
dingshujie <913292259@qq.com>
2020-10-15 22:56:54 +0800

    建表语句修改及新加员工-角色表

--- user/src/main/resources/user.sql
+++ user/src/main/resources/user.sql
@@ -10,64 +10,71 @@ CREATE TABLE `hqc_company` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='公司信息表';
 
 CREATE TABLE `hqc_company_department` (
-  `companyId` int(11) NOT NULL COMMENT '公司id',
   `departmentId` int(11) NOT NULL AUTO_INCREMENT COMMENT '部门id',
+	`companyId` int(11) NOT NULL COMMENT '公司id',
   `departmentName` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '部门名称',
   `departmentPhone` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '部门电话',
   `departmentDuty` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '部门职责',
-  PRIMARY KEY (`companyId`,`departmentId`)
+  PRIMARY KEY (`departmentId`,`companyId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='公司部门信息表';
 
 CREATE TABLE `hqc_department_user` (
-  `departmentId` int(11) NOT NULL COMMENT '部门id',
   `userId` int(11) NOT NULL AUTO_INCREMENT COMMENT '员工id',
+	`departmentId` int(11) NOT NULL COMMENT '部门id',
   `userName` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '员工名称',
   `userSex` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '员工性别',
-  `userBirthday` datetime(60)  NOT NULL COMMENT '员工生日',
+  `userBirthday` datetime  NOT NULL COMMENT '员工生日',
   `userAccount` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '员工账号',
   `userPassword` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '员工密码',
   `valid` varchar(1) COLLATE utf8_unicode_ci DEFAULT 'Y' COMMENT 'Y:在职/N:离职',
-  PRIMARY KEY (`departmentId`,`userId`)
+  PRIMARY KEY (`userId`,`departmentId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='部门员工信息表';
 
 CREATE TABLE `hqc_role` (
-  `companyId` int(11) NOT NULL '公司id',
-  `roleId` int(11) NOT NULL AUTO_INCREMENT COMMENT COMMENT '角色id',
+  `roleId` int(11) NOT NULL AUTO_INCREMENT  COMMENT '角色id',
+	`companyId` int(11) NOT NULL COMMENT '公司id',
   `roleName` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '角色名称',
   `valid` varchar(1) COLLATE utf8_unicode_ci DEFAULT 'Y' COMMENT 'Y:有效/N:无效',
-  PRIMARY KEY (`companyId`,`roleId`)
+  PRIMARY KEY (`roleId`,`companyId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='角色信息表';
 
-CREATE TABLE `hqc_permission` (
+CREATE TABLE `hqc_user_role` (
   `companyId` int(11) NOT NULL COMMENT '公司id',
+  `userId` int(11) NOT NULL   COMMENT '员工id',
+	`roleId` int(11) NOT NULL COMMENT '角色id',
+  PRIMARY KEY (`companyId`,`userId`,`roleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='员工角色表';
+
+CREATE TABLE `hqc_permission` (
   `permissionId` int(11) NOT NULL AUTO_INCREMENT COMMENT '权限id',
+	`companyId` int(11) NOT NULL COMMENT '公司id',
   `permissionName` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '权限名称',
   `permissionUrl` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '权限链接',
-  `permissionGroupId` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '权限组'
-  PRIMARY KEY (`companyId`,`permissionId`)
+  `permissionGroupId` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '权限组',
+  PRIMARY KEY (`permissionId`,`companyId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='权限表';
 
 CREATE TABLE `hqc_permission_group` (
-  `companyId` int(11) NOT NULL '公司id',
   `permissionGroupId` int(11) NOT NULL AUTO_INCREMENT COMMENT '权限组id',
+	`companyId` int(11) NOT NULL COMMENT '公司id',
   `permissionGroupName` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '权限组名称',
-  PRIMARY KEY (`companyId`,`permissionGroupId`)
+  PRIMARY KEY (`permissionGroupId` ,`companyId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='权限组表';
 
 CREATE TABLE `hqc_menu` (
-  `companyId` int(11) NOT NULL AUTO_INCREMENT COMMENT '公司id',
   `menuId` int(11) NOT NULL AUTO_INCREMENT COMMENT '菜单id',
+	`companyId` int(11) NOT NULL  COMMENT '公司id',
   `menuName` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '菜单名称',
   `menuUrl` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '菜单链接',
   `menuGroup` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '菜单组',
-  PRIMARY KEY (`companyId`,`menuId`)
+  PRIMARY KEY (`menuId`,`companyId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='菜单表';
 
 CREATE TABLE `hqc_menu_Group` (
   `companyId` int(11) NOT NULL COMMENT '公司id',
   `menuGroupId` int(11) NOT NULL AUTO_INCREMENT COMMENT '菜单组id',
   `menuGroupName` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '菜单组名称',
-  PRIMARY KEY (`companyId`,`menuGroupId`)
+  PRIMARY KEY (`menuGroupId`,`companyId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='菜单组表';
 
 -- 菜单权限
