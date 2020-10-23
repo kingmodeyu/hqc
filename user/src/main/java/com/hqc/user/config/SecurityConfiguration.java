988ecf0febe11c77274b529c83bc46f9a3130a34
dingshujie <913292259@qq.com>
2020-10-23 22:50:10 +0800

    spring Security了解

---
+++ user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.hqc.user.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+}
