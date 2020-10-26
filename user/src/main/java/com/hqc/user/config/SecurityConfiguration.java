ec6b1c1d5b5fc20d83deb30ff3faaac2ba39fcec
dingshujie <913292259@qq.com>
2020-10-26 20:06:56 +0800

    spring Security配置类

--- user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
+++ user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
@@ -1,9 +1,29 @@
 package com.hqc.user.config;
 
+import com.hqc.user.service.IHqcDepartmentUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private IHqcDepartmentUserService hqcDepartmentUserService;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.(hqcDepartmentUserService).passwordEncoder(new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return s.equals(charSequence.toString());
+            }
+        });
+    }
 
 }
