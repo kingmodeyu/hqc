90e27bde2b9b80c516cf646efd1f1b1b5e70da32
dingshujie <913292259@qq.com>
2020-10-27 22:24:59 +0800

    注入权限service

--- user/src/main/java/com/hqc/user/service/impl/HqcDepartmentUserServiceImpl.java
+++ user/src/main/java/com/hqc/user/service/impl/HqcDepartmentUserServiceImpl.java
@@ -4,6 +4,10 @@ import com.hqc.user.entity.HqcDepartmentUser;
 import com.hqc.user.mapper.HqcDepartmentUserMapper;
 import com.hqc.user.service.IHqcDepartmentUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hqc.user.service.IHqcRolePermissionService;
+import com.hqc.user.service.IHqcUserPermissionService;
+import com.hqc.user.service.IHqcUserRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +20,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HqcDepartmentUserServiceImpl extends ServiceImpl<HqcDepartmentUserMapper, HqcDepartmentUser> implements IHqcDepartmentUserService {
+
+    @Autowired
+    private IHqcUserRoleService userRoleService;
+
+    @Autowired
+    private IHqcUserPermissionService userPermissionService;
+
+    @Autowired
+    private IHqcRolePermissionService rolePermissionService;
+
 
 }
