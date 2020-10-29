43953403ed44e0c9525c587fa28548534bbdccca
dingshujie <913292259@qq.com>
2020-10-29 22:31:36 +0800

    发现不能用代码生成的接口

--- user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
+++ user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
@@ -2,28 +2,25 @@ package com.hqc.user.config;
 
 import com.hqc.user.service.IHqcDepartmentUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private IHqcDepartmentUserService hqcDepartmentUserService;
+    private HqcDepartmentUserServiceImpl hqcDepartmentUserService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.(hqcDepartmentUserService).passwordEncoder(new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence charSequence) {
-                return charSequence.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence charSequence, String s) {
-                return s.equals(charSequence.toString());
-            }
-        });
+        auth.userDetailsService(hqcDepartmentUserService).passwordEncoder(){
+        }
     }
 
 }
