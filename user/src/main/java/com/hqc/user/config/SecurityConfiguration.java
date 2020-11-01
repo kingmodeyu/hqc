f9d7c40ece8d03f4c50b7b7851e1dc740a2e45b3
dingshujie <913292259@qq.com>
2020-11-01 22:03:21 +0800

    了解中

--- user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
+++ user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.hqc.user.config;
 
 import com.hqc.user.service.IHqcDepartmentUserService;
+import com.hqc.user.service.IHqcUserPermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,6 +16,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private IHqcDepartmentUserService hqcDepartmentUserService;
+
+    @Autowired
+    private IHqcUserPermissionService userPermissionService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
