6992b4b346a8a54ac2dfce7ca01f94f86256939d
dingshujie <913292259@qq.com>
2020-11-03 20:57:15 +0800

    注入权限

--- user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
+++ user/src/main/java/com/hqc/user/config/SecurityConfiguration.java
@@ -7,8 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -16,7 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private IHqcDepartmentUserService hqcDepartmentUserService;
+    private MyUserDetailService userDetailService;
 
     @Autowired
     private IHqcUserPermissionService userPermissionService;
@@ -27,6 +31,45 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+        auth.userDetailsService(userDetailService).passwordEncoder(new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return s.equals(charSequence.toString());
+            }
+        });
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // 如果有允许匿名的url，填在下面
+//                .antMatchers().permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // 设置登陆页
+                .formLogin().loginPage("/login")
+                // 设置登陆成功页
+                .defaultSuccessUrl("/").permitAll()
+                // 自定义登陆用户名和密码参数，默认为username和password
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+                .and()
+                .logout().permitAll();
+
+        // 关闭CSRF跨域
+        http.csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 设置拦截忽略文件夹，可以对静态资源放行
+        web.ignoring().antMatchers("/css/**", "/js/**");
     }
 
 }
